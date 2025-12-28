2625efe79005995a9118313474364edd1a2be09f
Sai Prakash Holi <holisaiprakash@Sais-MacBook-Air.local>
2025-12-28 22:20:13 +0530

    pre render fix

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, Inject, PLATFORM_ID, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { Header } from './core/header/header';
 import { Footer } from "./core/footer/footer";
 import { BankBrand } from "./core/bank-brand/bank-brand";
@@ -9,6 +9,7 @@ import { LanguageService } from './services/language.service';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [
     RouterOutlet,
     CommonModule,
@@ -20,11 +21,22 @@ import { LanguageService } from './services/language.service';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('ilkal-co-op-bank');
-  constructor(private langService: LanguageService) {
-    const browserLang = navigator.language.startsWith('kn') ? 'kn' : 'en';
-    this.langService.loadLanguage(browserLang);
-  }
+export class App implements OnInit {
 
+  protected readonly title = signal('ilkal-co-op-bank');
+
+  constructor(
+    private langService: LanguageService,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) { }
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const browserLang = navigator.language?.startsWith('kn') ? 'kn' : 'en';
+      this.langService.loadLanguage(browserLang);
+    } else {
+      // SSR fallback
+      this.langService.loadLanguage('en');
+    }
+  }
 }
