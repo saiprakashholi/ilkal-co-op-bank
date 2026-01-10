22b6fca43619afb512b0095b9043399a609d92a8
Sai Prakash Holi <holisaiprakash@Sais-MacBook-Air.local>
2026-01-10 16:58:54 +0530

    Recource link added

--- src/app/core/footer/footer.ts
+++ src/app/core/footer/footer.ts
@@ -15,26 +15,26 @@ export class Footer implements OnInit {
   visitorsToday = 0;
 
   bankDetails = {
-  name: 'Ilkal Co-Operative Bank Ltd.',
-  address:
-    'Near Basavangudi, Ilkal – 587125, Dist: Bagalakot, Karnataka, India',
+    name: 'Ilkal Co-Operative Bank Ltd.',
+    address:
+      'Near Basavangudi, Ilkal – 587125, Dist: Bagalakot, Karnataka, India',
 
-  ifsc: 'UTIB0SICB25',
+    ifsc: 'UTIB0SICB25',
 
-  workingHours: 'Monday – Saturday: 10:30 AM – 4:30 PM',
+    workingHours: 'Monday – Saturday: 10:30 AM – 4:30 PM',
 
-  gst: '29AAAAI1234A1Z5',
-  bankRegNo: 'UB/KA/721 (08-12-1986)',
-  rbiLicenceNo: '22530 (24-08-1965)',
-  email: 'info@ilkalbank.com',
-  website: 'https://www.ilkalbank.com',
-};
+    gst: '29AAAAI1234A1Z5',
+    bankRegNo: 'UB/KA/721 (08-12-1986)',
+    rbiLicenceNo: '22530 (24-08-1965)',
+    email: 'info@ilkalbank.com',
+    website: 'https://www.ilkalbank.com',
+  };
 
 
   constructor(
     // private visitorSvc: VisitorService,
     public lang: LanguageService
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     // this.visitorsToday = this.visitorSvc.incrementToday();
