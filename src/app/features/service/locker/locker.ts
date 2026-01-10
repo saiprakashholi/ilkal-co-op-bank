58524cd6dd9b6dd149243daeb7960b27421c49eb
Sai Prakash Holi <holisaiprakash@Sais-MacBook-Air.local>
2026-01-11 00:16:13 +0530

    Updated locker

--- src/app/features/service/locker/locker.ts
+++ src/app/features/service/locker/locker.ts
@@ -16,23 +16,23 @@ export class Locker {
   lockers = [
     {
       size: 'SMALL',
-      dimensions: '0.5 × 0.70 × 1.75',
-      rent: '1000 + GST'
+      // dimensions: '0.5 × 0.70 × 1.75',
+      // rent: '1000 + GST'
     },
     {
       size: 'MEDIUM',
-      dimensions: '0.7 × 0.70 × 1.75',
-      rent: '1500 + GST'
+      // dimensions: '0.7 × 0.70 × 1.75',
+      // rent: '1500 + GST'
     },
     {
       size: 'BIG',
-      dimensions: '0.75 × 1.00 × 1.75',
-      rent: '3000 + GST'
+      // dimensions: '0.75 × 1.00 × 1.75',
+      // rent: '3000 + GST'
     },
     {
       size: 'LARGE',
-      dimensions: '1.5 × 1.65 × 1.75',
-      rent: '4500 + GST'
+      // dimensions: '1.5 × 1.65 × 1.75',
+      // rent: '4500 + GST'
     }
   ];
 }
