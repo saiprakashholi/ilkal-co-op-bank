71e7037691f96b1318babcc2b776ebf7c4e6bfdf
Sai Prakash Holi <holisaiprakash@Sais-MacBook-Air.local>
2026-02-06 23:20:20 +0530

    deposit interest rates updated

--- src/app/features/deposits/interest-rate/interest-rate.ts
+++ src/app/features/deposits/interest-rate/interest-rate.ts
@@ -14,8 +14,8 @@ export class InterestRate {
     { title: '46 – 90 Days', rate: 5.5 },
     { title: '91 – 180 Days', rate: 6.5 },
     { title: '181 – 364 Days', rate: 7.0 },
-    { title: '12 – 35 Months', rate: 8.5 },
+    { title: '12 – 35 Months', rate: 8.25 },
     { title: '36 Months & Above', rate: 8.5 },
-    { title: 'Bulk Deposit', rate: 9.0 },
+    { title: 'Bulk Deposit', rate: 8.75 },
   ];
 }
